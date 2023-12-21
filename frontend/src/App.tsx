6d4dc6a173669f995d9f4bb8f77f03a201cc92c3
AurelienEbeng <ebengaurelien@gmail.com>
2023-12-21 09:33:07 -0500

    creating navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import {useContext} from 'react'
-import { ThemeContext } from './context/theme.context'
+import { useContext } from "react";
+import { ThemeContext } from "./context/theme.context";
+import Navbar from "./components/navbar/Navbar.component";
 
 const App = () => {
-  const {darkMode} = useContext(ThemeContext);
-  
-  const appStyles = darkMode ?"app dark":"app"
+  const { darkMode } = useContext(ThemeContext);
+
+  const appStyles = darkMode ? "app dark" : "app";
 
   return (
-    <div className={appStyles}>App</div>
-  )
-}
+    <div className={appStyles}>
+      <Navbar />
+      <div className="wrapper">Routes</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -0,0 +1,14 @@
+import './navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="brand">
+            <span>Bug Tracker</span>
+        </div>
+        <div className="menu"></div>
+    </div>
+  )
+}
+
+export default Navbar
