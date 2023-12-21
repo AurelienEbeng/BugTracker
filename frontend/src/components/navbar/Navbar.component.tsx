c793865a46d82e9b7ba48ba4a94f4059579cd220
AurelienEbeng <ebengaurelien@gmail.com>
2023-12-21 11:29:07 -0500

    worked on navbar. Added dark mode button. Worked on navbar.scss, added the styling for the desktop version dark mode and light mode.

--- frontend/src/components/navbar/Navbar.component.tsx
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -1,14 +1,50 @@
-import './navbar.scss'
+import { useContext } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { ThemeContext } from "../../context/theme.context";
 
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/employees", label: "Employees" },
+  { href: "/projects", label: "Projects" },
+  { href: "/roles", label: "Roles" },
+  { href: "/Tickets", label: "Tickets" },
+  { href: "/TicketAttachments", label: "TicketAttachments" },
+  { href: "/TicketComments", label: "TicketComments" },
+  { href: "/TicketHistories", label: "TicketHistories" },
+];
 const Navbar = () => {
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
   return (
-    <div className='navbar'>
-        <div className="brand">
-            <span>Bug Tracker</span>
-        </div>
-        <div className="menu"></div>
+    <div className="navbar">
+      <div className="brand">
+        <span>Bug Tracker</span>
+      </div>
+      <div className="menu">
+        <ul>
+          {links.map((item) => (
+            <li key={item.href}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Menu />
+      </div>
+      <div className="toggle">
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
+        </ToggleButton>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
