756f22a527056e30a0266b29c88816cb34c35d05
AurelienEbeng <ebengaurelien@gmail.com>
2024-01-14 08:39:51 -0500

    You can now download a files in the ticket atttachment table

--- frontend/src/components/tickets/TicketAttachmentsGrid.component.tsx
+++ frontend/src/components/tickets/TicketAttachmentsGrid.component.tsx
@@ -1,12 +1,28 @@
 import { Box } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { ITicketAttachment } from "../../types/global.typing";
+import { baseUrl } from "../../constants/url.constants";
+import { PictureAsPdf } from "@mui/icons-material";
 
 const column: GridColDef[] = [
   { field: "notes", headerName: "Notes", width: 200 },
   { field: "uploaderName", headerName: "Uploader Name", width: 200 },
 
-  { field: "fileUrl", headerName: "File Url", width: 200 },
+  {
+    field: "fileUrl",
+    headerName: "Download",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <a
+          href={`${baseUrl}/TicketAttachment/download/${params.row.fileUrl}`}
+          download
+        >
+          <PictureAsPdf />
+        </a>
+      );
+    },
+  },
 ];
 
 interface ITicketAttachmentsGridProps {
