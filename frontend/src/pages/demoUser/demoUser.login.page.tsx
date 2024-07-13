7540c8984b44cb29259080bcf09c55d13f4edf2d
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-07-13 08:31:56 -0400

    Built the UI of the Demo User Login component

--- frontend/src/pages/demoUser/demoUser.login.page.tsx
+++ frontend/src/pages/demoUser/demoUser.login.page.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+import "./demoUser.scss"
 
 const DemoUserLogin = () => {
   return (
     <div className='content'>
         <div className='demoUsers'>
-            <div className='demoUser'>
-                <img />
-                <p>Demo Admin</p>
+            <h1>Demo Users Login</h1>
+            <div className='row'>
+                <div className='demoUser'>
+                    <PersonIcon style={{fontSize:"100px"}}/>
+                    <p>Admin</p>
+                </div>
+                <div className='demoUser'>
+                    <PersonIcon color='warning' style={{fontSize:"100px"}} />
+                    <p>Project Manager</p>
+                </div>
             </div>
+            <div className='row'>
+                <div className='demoUser'>
+                    <PersonIcon color='success' style={{fontSize:"100px"}}/>
+                    <p>Developer</p>
+                </div>
+                <div className='demoUser'>
+                    <PersonIcon color='primary' style={{fontSize:"100px"}}/>
+                    <p>Quality Assurance</p>
+                </div>
+            </div>
+            <div className='extra'><p>Have an account? Login</p></div>
         </div>
     </div>
   )
