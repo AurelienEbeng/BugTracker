1a3c13bfe779b43a77d42a74c8a1ba82889501e4
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-07-13 08:48:05 -0400

    The Login link leads to the SignIn page. The icon links all lead to the home page

--- frontend/src/pages/demoUser/demoUser.login.page.tsx
+++ frontend/src/pages/demoUser/demoUser.login.page.tsx
@@ -1,35 +1,43 @@
-import PersonIcon from '@mui/icons-material/Person';
-import "./demoUser.scss"
+import PersonIcon from "@mui/icons-material/Person";
+import "./demoUser.scss";
+import { Link } from "react-router-dom";
 
 const DemoUserLogin = () => {
   return (
-    <div className='content'>
-        <div className='demoUsers'>
-            <h1>Demo Users Login</h1>
-            <div className='row'>
-                <div className='demoUser'>
-                    <PersonIcon style={{fontSize:"100px"}}/>
-                    <p>Admin</p>
-                </div>
-                <div className='demoUser'>
-                    <PersonIcon color='warning' style={{fontSize:"100px"}} />
-                    <p>Project Manager</p>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='demoUser'>
-                    <PersonIcon color='success' style={{fontSize:"100px"}}/>
-                    <p>Developer</p>
-                </div>
-                <div className='demoUser'>
-                    <PersonIcon color='primary' style={{fontSize:"100px"}}/>
-                    <p>Quality Assurance</p>
-                </div>
-            </div>
-            <div className='extra'><p>Have an account? Login</p></div>
+    <div className="content">
+      <div className="demoUsers">
+        <h1>Demo Users Login</h1>
+        <div className="row">
+          <div className="demoUser">
+            <Link to ="/"><PersonIcon style={{ fontSize: "100px", color:"black" }} /></Link>
+            <p>Admin</p>
+          </div>
+          <div className="demoUser">
+          <Link to ="/"><PersonIcon color="warning" style={{ fontSize: "100px" }} /></Link>
+            <p>Project Manager</p>
+          </div>
         </div>
+        <div className="row">
+          <div className="demoUser">
+          <Link to ="/"><PersonIcon color="success" style={{ fontSize: "100px" }} /></Link>
+            <p>Developer</p>
+          </div>
+          <div className="demoUser">
+          <Link to ="/"><PersonIcon color="primary" style={{ fontSize: "100px" }} /></Link>
+            <p>Quality Assurance</p>
+          </div>
+        </div>
+        <div className="extra">
+          <p>
+            Have an account?{" "}
+            <Link to="/signIn"  >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DemoUserLogin
+export default DemoUserLogin;
