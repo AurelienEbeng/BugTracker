ddf3112cbce544d647395cfab608f986b89bb54b
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-08-27 14:07:41 -0400

    Built the UI for TicketDetailsEdit.Created handleClickUpdateBtn

--- frontend/src/pages/tickets/TicketDetailsEdit.tsx
+++ frontend/src/pages/tickets/TicketDetailsEdit.tsx
@@ -1,13 +1,203 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import {
+  Button,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { ITicket } from "../../types/global.typing";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useJwt } from "../../context/Jwt.context";
+import httpModule from "../../helpers/http.module";
+import "./tickets.scss";
 
-const TicketDetailsEdit = () => {
-    const location = useLocation()
-    const {ticket} = location.state
-    console.log(ticket)
-  return (
-    <div>Ticket Edit Details</div>
-  )
+interface IProjectMember {
+  projectId: string;
+  memberId: string;
+  dateAdded: string;
+  username: string;
 }
+const TicketDetailsEdit = () => {
+  const location = useLocation();
+  const { ticket } = location.state;
+  const [updatedTicket, setUpdatedTicket] = useState<ITicket>(ticket);
+  const jwt = useJwt();
+  const redirect = useNavigate();
+  const types = [
+    "BUGS",
+    "FEATURES_REQUEST",
+    "OTHER_COMMENTS",
+    "DOCUMENTS_REQUEST",
+  ];
+  const [projectMembers, setProjectMembers] = useState<IProjectMember[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-export default TicketDetailsEdit
+  const priority = ["HIGH", "MEDIUM", "LOW", "NONE"];
+  const status =["NEW", "OPEN", "IN_PROGRESS", "RESOLVED", "ADDITIONAL_INFO_REQUIRED"]
+
+  useEffect(() => {
+    setLoading(true);
+    if (!jwt.isLoggedIn()) {
+      redirect("/signIn", { replace: true });
+      return;
+    }
+
+    let params = new URLSearchParams();
+    params.append("projectId", updatedTicket.projectId);
+    httpModule
+      .get<IProjectMember[]>("ProjectMember/Get?" + params, {
+        headers: { Authorization: "Bearer " + jwt.user.jwtToken },
+      })
+      .then((response) => {
+        setProjectMembers(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Error. Check console");
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  
+    function handleClickUpdateBtn() {
+      if (
+        ticket.title === "" ||
+        ticket.description === "" 
+      ) {
+        alert("Fill all fields");
+        return;
+      }
+  
+      setLoading(true);
+  
+      httpModule
+        .put("/Ticket/Update", updatedTicket, {
+          headers: { Authorization: "Bearer " + jwt.user.jwtToken },
+        })
+        .then(() => {
+          setLoading(false);
+          redirect("/projects/ticket", { state: { ticket:{...updatedTicket} } });
+        })
+        .catch((error) => {
+          alert("Error, check console");
+          console.log(error);
+          setLoading(false);
+        });
+    } 
+  return (
+    <div className="content">
+      {loading ? (
+        <CircularProgress size={100} />
+      ) : (
+        <div className="add-ticket">
+          <div className="heading">
+            <h1> Edit Ticket</h1>
+          </div>
+          <TextField
+            label="Title"
+            variant="outlined"
+            value={ticket.title}
+            onChange={(e) =>
+              setUpdatedTicket({ ...ticket, title: e.target.value })
+            }
+            fullWidth
+          />
+
+          <TextField
+            label="Description"
+            variant="outlined"
+            value={ticket.description}
+            onChange={(e) =>
+              setUpdatedTicket({ ...ticket, description: e.target.value })
+            }
+            multiline
+            fullWidth
+          />
+
+          <FormControl fullWidth>
+            <InputLabel>Type</InputLabel>
+            <Select
+              value={ticket.type}
+              variant="outlined"
+              onChange={(e) =>
+                setUpdatedTicket({ ...ticket, type: e.target.value })
+              }
+            >
+              {types.map((type) => (
+                <MenuItem key={type} value={type}>
+                  {type}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth>
+            <InputLabel>Assigned Developer</InputLabel>
+            <Select
+              value={ticket.assignedDeveloperId}
+              variant="outlined"
+              onChange={(e) =>
+                setUpdatedTicket({
+                  ...ticket,
+                  assignedDeveloperId: e.target.value,
+                })
+              }
+            >
+              {projectMembers.map((projectMember) => (
+                <MenuItem
+                  key={projectMember.memberId}
+                  value={projectMember.memberId}
+                >
+                  {projectMember.username}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth>
+            <InputLabel>Status</InputLabel>
+            <Select
+              variant="outlined"
+              value={updatedTicket.status}
+              onChange={(e) =>
+                setUpdatedTicket({ ...ticket, status: e.target.value })
+              }
+            >
+              {status.map((s) => (
+                <MenuItem key={s} value={s}>
+                  {s}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth>
+            <InputLabel>Priority</InputLabel>
+            <Select
+              variant="outlined"
+              value={updatedTicket.priority}
+              onChange={(e) =>
+                setUpdatedTicket({ ...ticket, priority: e.target.value })
+              }
+            >
+              {priority.map((p) => (
+                <MenuItem key={p} value={p}>
+                  {p}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <div className="btns">
+            <Button variant="outlined" onClick={handleClickUpdateBtn}>Update</Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TicketDetailsEdit;
