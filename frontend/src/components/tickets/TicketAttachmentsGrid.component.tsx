327b134c7860ecf5c83515267cd832295e8f71ff
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-09-01 11:46:39 -0400

    Added the flex property ot TicketAttachmentGrid. It uses dynamic row height. Its rows' content and borders have space

--- frontend/src/components/tickets/TicketAttachmentsGrid.component.tsx
+++ frontend/src/components/tickets/TicketAttachmentsGrid.component.tsx
@@ -1,5 +1,5 @@
 import { Box, Button } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, gridClasses, GridColDef } from "@mui/x-data-grid";
 import { ITicketAttachment } from "../../types/global.typing";
 import { PictureAsPdf } from "@mui/icons-material";
 import httpModule from "../../helpers/http.module";
@@ -35,13 +35,14 @@ const TicketAttachmentsGrid = ({ data }: ITicketAttachmentsGridProps) => {
   };
 
   const column: GridColDef[] = [
-    { field: "notes", headerName: "Notes", width: 200 },
-    { field: "uploaderName", headerName: "Uploader Name", width: 200 },
+    { field: "notes", headerName: "Notes", flex:1 },
+    { field: "uploaderName", headerName: "Uploader Name", width: 200, flex:1 },
 
     {
       field: "fileUrl",
       headerName: "Download",
       width: 150,
+      flex:1,
       renderCell: (params) => {
         return (
           <Button onClick={() => handleDownload(params.row.fileUrl)}>
@@ -52,12 +53,17 @@ const TicketAttachmentsGrid = ({ data }: ITicketAttachmentsGridProps) => {
     },
   ];
   return (
-    <Box sx={{ width: "100%", height: 200 }} className="tickets-grid">
+    <Box sx={{ width: "100%", height: "60%" }} className="tickets-grid">
       <DataGrid
         rows={data}
         columns={column}
         getRowId={(row) => row.id}
-        rowHeight={50}
+        getRowHeight={()=>'auto'}
+        sx={{
+          [`& .${gridClasses.cell}`]: {
+            py: 2,
+          },
+        }}
       />
     </Box>
   );
