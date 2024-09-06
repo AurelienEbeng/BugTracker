0d6b5a2dc7e2d37c3ec9143567e117a2a4758c89
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-09-06 19:23:26 -0400

    Added an Unassigned Select field in ManageAssignedPersonnel. It also calls GetUnassignedPersonnel api

--- frontend/src/pages/manageAssignedPersonnel/ManageAssignedPersonnel.tsx
+++ frontend/src/pages/manageAssignedPersonnel/ManageAssignedPersonnel.tsx
@@ -3,8 +3,13 @@ import AssignedPersonnelGrid from "../../components/manageAssignedPersonnel/Assi
 import { useJwt } from "../../context/Jwt.context";
 import { useLocation, useNavigate } from "react-router-dom";
 import httpModule from "../../helpers/http.module";
-import { CircularProgress } from "@mui/material";
+import { Button, CircularProgress, MenuItem, TextField } from "@mui/material";
+import { IEmployee } from "../../types/global.typing";
 
+type ProjectMember = {
+  projectId: string;
+  memberId: string;
+};
 const ManageAssignedPersonnel = () => {
   const [assignedPersonnel, setAssignedPersonnel] = useState([]);
   const [loading, setLoading] = useState<boolean>(false);
@@ -12,6 +17,14 @@ const ManageAssignedPersonnel = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { project } = location.state;
+  const [projectMember, setProjectMember] = useState<ProjectMember>(
+    {} as ProjectMember
+  );
+  const [unassignedPersonnel, setUnassignedPersonnel] = useState<IEmployee[]>(
+    []
+  );
+  const [loadingUnassignedDeveloper, setLoadingUnassignedDeveloper] =
+    useState<boolean>(false);
 
   useEffect(() => {
     if (!jwt.isLoggedIn()) {
@@ -19,9 +32,11 @@ const ManageAssignedPersonnel = () => {
       return;
     }
     setLoading(true);
+    setLoadingUnassignedDeveloper(true);
+    setProjectMember({ ...projectMember, projectId: project.id });
 
-    let params = new URLSearchParams()
-    params.append("projectId",project.id)
+    let params = new URLSearchParams();
+    params.append("projectId", project.id);
     httpModule
       .get("/ProjectMember/GetAssignedPersonnel?" + params, {
         headers: { Authorization: "Bearer " + jwt.user.jwtToken },
@@ -35,14 +50,49 @@ const ManageAssignedPersonnel = () => {
         console.log(error.response);
         setLoading(false);
       });
+
+    httpModule
+      .get("/ProjectMember/GetUnassignedPersonnel?" + params, {
+        headers: { Authorization: "Bearer " + jwt.user.jwtToken },
+      })
+      .then((response) => {
+        setUnassignedPersonnel(response.data);
+        setLoadingUnassignedDeveloper(false);
+      })
+      .catch((error) => {
+        alert("Error, check the console");
+        console.log(error.response);
+        setLoadingUnassignedDeveloper(false);
+      });
   }, []);
+
+  function handleClickAddBtn() {}
   return (
     <div>
-      {loading ? (
+      {loading || loadingUnassignedDeveloper ? (
         <CircularProgress size={100} />
       ) : (
         <>
-        <div className="heading">{project.name}'s Personnel</div>
+          <div className="heading">{project.name}'s Personnel</div>
+          <div className="unassignedPersonnel">
+            <TextField
+              variant="outlined"
+              fullWidth
+              select
+              label="Unassigned Developer"
+              value={projectMember.memberId}
+              onChange={(e) =>
+                setProjectMember({ ...projectMember, memberId: e.target.value })
+              }
+            >
+              {unassignedPersonnel.map((user) => (
+                <MenuItem key={user.id} value={user.id}>
+                  {user.name}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button variant="outlined" onClick={handleClickAddBtn}>Add</Button>
+          </div>
           <AssignedPersonnelGrid data={assignedPersonnel} />
         </>
       )}
