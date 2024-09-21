aab322a8459010aac2bd34d67bb6b162f78dac17
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-09-21 07:51:12 -0400

    Modified TicketsGrid

--- frontend/src/components/tickets/TicketsGrid.component.tsx
+++ frontend/src/components/tickets/TicketsGrid.component.tsx
@@ -1,23 +1,36 @@
 import { Box } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, gridClasses, GridColDef } from "@mui/x-data-grid";
 import { ITicket } from "../../types/global.typing";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const column: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 50 },
-  { field: "title", headerName: "Title", width: 200 },
-  { field: "description", headerName: "Descriptionn", width: 200 },
+  { field: "title", headerName: "Title", width: 200, flex:1 },
   {
     field: "dateCreated",
     headerName: "Creation Time",
     width: 200,
-    renderCell: (params) => moment(params.row.dateJoined).format("YYYY-MM--DD"),
+    renderCell: (params) => moment(params.row.dateCreated).format("YYYY-MM-DD"),
+    flex:1
   },
-  { field: "status", headerName: "Status", width: 100 },
-  { field: "type", headerName: "Type", width: 100 },
-  { field: "priority", headerName: "Priority", width: 70 },
-  { field: "projectId", headerName: "Project Id", width: 50 },
-  { field: "projectName", headerName: "Project Name", width: 200 },
+  { field: "priority", headerName: "Priority", width: 200, flex:1 },
+  { field: "projectName", headerName: "Project Name", width: 200, flex:1 },
+  {
+    field: "",
+    flex:1,
+    renderCell: (params) => {
+      return (
+        <Link
+          to="/projects/ticket"
+          state={{
+            ticketId: `${params.row.id}`,
+          }}
+        >
+          Details
+        </Link>
+      );
+    },
+  },
 ];
 
 interface ITicketsGridProps {
@@ -26,12 +39,17 @@ interface ITicketsGridProps {
 
 const TicketsGrid = ({ data }: ITicketsGridProps) => {
   return (
-    <Box sx={{ width: "100%", height: 200 }} className="tickets-grid">
+    <Box sx={{ width: "100%", height: 450 }} className="tickets-grid">
       <DataGrid
         rows={data}
         columns={column}
         getRowId={(row) => row.id}
-        rowHeight={50}
+        getRowHeight={() => "auto"}
+        sx={{
+          [`& .${gridClasses.cell}`]: {
+            py: 2,
+          },
+        }}
       />
     </Box>
   );
