8f5d66959043436ce669832b504b54bce2e60865
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-10-25 10:47:08 -0400

    Created NotificationController. Created the GetMyNotfications route

---
+++ backend/backend/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using backend.Core.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin,Developer,QualityAssurance")]
+    public class NotificationController : ControllerBase
+    {
+        private ApplicationDBContext _context { get; }
+
+        public NotificationController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // Read
+        [HttpGet]
+        [Route("GetMyNotifications")]
+        public async Task<ActionResult> GetMyNotfications(string userId)
+        {
+            var notifications = from n in _context.Notifications
+                                where n.ReceiverId == userId && n.IsRead == false
+                                select new
+                                {
+                                    id = n.Id,
+                                    receiverId = n.ReceiverId,
+                                    isRead = n.IsRead,
+                                    message = n.Message
+                                };
+            return Ok(notifications);
+        }
+    }
+}
