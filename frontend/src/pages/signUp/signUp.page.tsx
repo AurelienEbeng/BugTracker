da6bb9e0e5c5e3ebefc52234475231793342a8e6
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-03 08:57:54 -0500

    Added buttons to Sign Up

--- frontend/src/pages/signUp/signUp.page.tsx
+++ frontend/src/pages/signUp/signUp.page.tsx
@@ -1,45 +1,55 @@
-import { TextField } from "@mui/material"
-import { useState } from "react"
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type User = {
-    name:string,
-    email:string,
-    password:string,
-    confirmPassword:string
-}
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const SignUp = () => {
-    const [user,setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>({} as User);
+  function handleCreateBtn() {}
   return (
     <div>
-        <TextField
+      <TextField
         fullWidth
         variant="outlined"
         label="Name"
-        onChange={e=> setUser({...user,name:e.target.value})}
-        />
-        <TextField
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
+      />
+      <TextField
         fullWidth
         variant="outlined"
         label="Email"
-        onChange={e=> setUser({...user,email:e.target.value})}
-        />
-        <TextField
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <TextField
         fullWidth
         variant="outlined"
         label="Password"
-        onChange={e=> setUser({...user,password:e.target.value})}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
         type="password"
-        />
-        <TextField
+      />
+      <TextField
         fullWidth
         variant="outlined"
         label="Confirm Password"
-        onChange={e=> setUser({...user,confirmPassword:e.target.value})}
+        onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })}
         type="password"
-        />
+      />
+      <div className="btns">
+        <Button variant="outlined">
+          <Link to="/signIn">Sign in</Link>
+        </Button>
+        <Button variant="outlined" onClick={handleCreateBtn}>
+          Create
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
