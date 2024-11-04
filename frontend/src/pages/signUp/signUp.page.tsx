20d82015eab2e4c6adb30c5120249c04e8488931
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-04 08:39:54 -0500

    Wrote the code for handleCreateBtn in SignUp.

--- frontend/src/pages/signUp/signUp.page.tsx
+++ frontend/src/pages/signUp/signUp.page.tsx
@@ -1,9 +1,15 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Button, InputAdornment, TextField } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import "./signUp.scss"
+import { Link, useNavigate } from "react-router-dom";
+import "./signUp.scss";
 import { ThemeContext } from "../../context/theme.context";
+import httpModule from "../../helpers/http.module";
 
 type User = {
   name: string;
@@ -13,12 +19,47 @@ type User = {
 };
 
 const SignUp = () => {
-  const [user, setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const { darkMode } = useContext(ThemeContext);
   const signUpBackground = darkMode ? "signUp dark" : "signUp";
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  function handleCreateBtn() {}
+  function handleCreateBtn() {
+    if (
+      user.name === "" ||
+      user.email === "" ||
+      user.password === "" ||
+      user.confirmPassword === ""
+    ) {
+      alert("Fill all fields");
+      return;
+    }
+    const emailRegex = /[a-z0-9]+@[a-z]+.[a-z]+/;
+    if (!emailRegex.test(user.email)) {
+      alert("Verify your email");
+      return;
+    }
+    if (user.password !== user.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    setLoading(true);
+
+    httpModule
+      .post("Employee/Create", user)
+      .then(() => navigate("/signIn"))
+      .catch((error) => {
+        alert("Error, check console");
+        console.log(error.response);
+      });
+  }
   const handleClickShowPassword = () => {
     setShowPassword((prevState: boolean) => {
       return !prevState;
@@ -30,77 +71,83 @@ const SignUp = () => {
   };
   return (
     <div className="content">
-      <div className={signUpBackground}>
-        <div className="heading"><h2>Sign Up</h2></div>
-        <div className="formFields">
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Name"
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-          value={user.name}
-          autoComplete="off"
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Email"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          value={user.email}
-          autoComplete="off"
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          type={showPassword ? "text" : "password"}
-          value={user.password}
-          autoComplete="off"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment
-                position="end"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-              >
-                {showPassword ? <Visibility /> : <VisibilityOff />}
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Confirm Password"
-          onChange={(e) =>
-            setUser({ ...user, confirmPassword: e.target.value })
-          }
-          type={showPassword ? "text" : "password"}
-          value={user.confirmPassword}
-          autoComplete="off"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment
-                position="end"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-              >
-                {showPassword ? <Visibility /> : <VisibilityOff />}
-              </InputAdornment>
-            ),
-          }}
-        />
+      {loading ? (
+        <CircularProgress size={100} />
+      ) : (
+        <div className={signUpBackground}>
+          <div className="heading">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="formFields">
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Name"
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+              value={user.name}
+              autoComplete="off"
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Email"
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              value={user.email}
+              autoComplete="off"
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Password"
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              type={showPassword ? "text" : "password"}
+              value={user.password}
+              autoComplete="off"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment
+                    position="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Confirm Password"
+              onChange={(e) =>
+                setUser({ ...user, confirmPassword: e.target.value })
+              }
+              type={showPassword ? "text" : "password"}
+              value={user.confirmPassword}
+              autoComplete="off"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment
+                    position="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <div className="btns">
+            <Button variant="outlined">
+              <Link to="/signIn">Sign in</Link>
+            </Button>
+            <Button variant="outlined" onClick={handleCreateBtn}>
+              Create
+            </Button>
+          </div>
         </div>
-        <div className="btns">
-          <Button variant="outlined">
-            <Link to="/signIn">Sign in</Link>
-          </Button>
-          <Button variant="outlined" onClick={handleCreateBtn}>
-            Create
-          </Button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
