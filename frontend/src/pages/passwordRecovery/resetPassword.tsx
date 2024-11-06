2d69fb13fead8cc25672640303b440921953821b
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-11-05 19:50:42 -0500

    ResetPassword will redirect you to the sign in page if its email and token are null. Wrote the code for handleSubmit in ResetPassword. Tested the functionality and the password was reset

--- frontend/src/pages/passwordRecovery/resetPassword.tsx
+++ frontend/src/pages/passwordRecovery/resetPassword.tsx
@@ -1,16 +1,28 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Button, InputAdornment, TextField } from "@mui/material";
-import { useState } from "react";
-import "./resetPassword.scss"
+import { useEffect, useState } from "react";
+import "./resetPassword.scss";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import httpModule from "../../helpers/http.module";
 
 const ResetPassword = () => {
+  const [searchParams, setSearchParmams] = useSearchParams();
   const [resetPassword, setResetPassword] = useState({
     password: "",
     confirmPassword: "",
-    email: "",
-    token: "",
+    email: searchParams.get("email"),
+    token: searchParams.get("token"),
   });
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (resetPassword.email == null || resetPassword.token == null) {
+      navigate("/signIn", { replace: true });
+      return;
+    }
+  }, []);
+
   const handleClickShowPassword = () => {
     setShowPassword((prevState: boolean) => {
       return !prevState;
@@ -20,55 +32,76 @@ const ResetPassword = () => {
   const handleMouseDownPassword = (event: any) => {
     event.preventDefault();
   };
-  function handleBtnSubmit(){}
+  function handleBtnSubmit() {
+    if (resetPassword.password == "" || resetPassword.confirmPassword == "") {
+      alert("Fill all fields");
+      return;
+    }
+    if (resetPassword.password != resetPassword.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    httpModule
+      .put("Employee/ResetPassword", resetPassword)
+      .then(() => navigate("/signIn"))
+      .catch((error) => {
+        alert("Error, check console");
+        console.log(error.response);
+      });
+  }
   return (
     <div className="content">
       <div className="resetPassword">
         <h2>Reset Password</h2>
 
         <TextField
-              fullWidth
-              variant="outlined"
-              label="Password"
-              onChange={(e) => setResetPassword({ ...resetPassword, password: e.target.value })}
-              type={showPassword ? "text" : "password"}
-              value={resetPassword.password}
-              autoComplete="off"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment
-                    position="end"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              fullWidth
-              variant="outlined"
-              label="Confirm Password"
-              onChange={(e) =>
-                setResetPassword({ ...resetPassword, confirmPassword: e.target.value })
-              }
-              type={showPassword ? "text" : "password"}
-              value={resetPassword.confirmPassword}
-              autoComplete="off"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment
-                    position="end"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </InputAdornment>
-                ),
-              }}
-            />
-        
+          fullWidth
+          variant="outlined"
+          label="Password"
+          onChange={(e) =>
+            setResetPassword({ ...resetPassword, password: e.target.value })
+          }
+          type={showPassword ? "text" : "password"}
+          value={resetPassword.password}
+          autoComplete="off"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Confirm Password"
+          onChange={(e) =>
+            setResetPassword({
+              ...resetPassword,
+              confirmPassword: e.target.value,
+            })
+          }
+          type={showPassword ? "text" : "password"}
+          value={resetPassword.confirmPassword}
+          autoComplete="off"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </InputAdornment>
+            ),
+          }}
+        />
 
         <Button
           variant="outlined"
